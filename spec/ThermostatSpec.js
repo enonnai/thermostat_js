0cc4b8756b2aa433ea1761df4188e019cbf457a9
Cristina Iannone <iannonecristina@gmail.com>
2017-05-11 12:10:52 +0100

    Thermostat has a temperature

---
+++ spec/ThermostatSpec.js
@@ -0,0 +1,12 @@
+describe("Thermostat", function(){
+var thermostat;
+
+beforeEach(function() {
+  thermostat = new Thermostat();
+});
+
+  it("starts at 20 degrees", function() {
+    expect(thermostat.getTemperature()).toEqual(20);
+  });
+
+});
---
+++ src/Thermostat.js
@@ -0,0 +1,4 @@
+function Thermostat(){ this.temperature = 20; }
+Thermostat.prototype.getTemperature = function () {
+return this.temperature;
+};
