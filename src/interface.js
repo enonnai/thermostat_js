7958daf860f893c5211278090545cd3406b419be
anaalta <suciuanamaria@yahoo.com>
2017-05-12 15:32:50 +0200

    functional, but not refactored - changes weather by city

--- src/interface.js
+++ src/interface.js
@@ -1,9 +1,21 @@
 $(document).ready(function() {
   var thermostat = new Thermostat();
-  $.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
-  $('#current-temperature').text(data.main.temp);
+  updateTemperature();
+  $('#current-city').change(function(){
+    var city = $('#current-city').val();
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+    $('#current-temperature').text(data.main.temp+' Celsius');
+  });
 });
-   updateTemperature();
+
+
+ //  function displayWeather(city) {
+ // var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+ // var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
+ // var units = '&units=metric';
+ // $.get(url + token + units, function(data) {
+ //   $('#current-temperature').text(data.main.temp);
+ // })
 
 $('#temperature-up').click(function() {
   thermostat.up();
