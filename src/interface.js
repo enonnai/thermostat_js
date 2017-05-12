ffbf98eeea6b24f051c3748aa43092f967360ee5
Cristina Iannone <iannonecristina@gmail.com>
2017-05-12 11:45:10 +0100

    Adds weather API in index and interface files

--- src/interface.js
+++ src/interface.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
   var thermostat = new Thermostat();
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+  $('#current-temperature').text(data.main.temp);
+});
    updateTemperature();
 
 $('#temperature-up').click(function() {
