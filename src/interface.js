0fd5a9d3ea594e9e2da349a6f62c1acea2da7c86
anaalta <suciuanamaria@yahoo.com>
2017-05-12 15:48:54 +0200

    finalized API challenge - weather by selected city

--- src/interface.js
+++ src/interface.js
@@ -8,15 +8,6 @@ $(document).ready(function() {
   });
 });
 
-
- //  function displayWeather(city) {
- // var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
- // var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
- // var units = '&units=metric';
- // $.get(url + token + units, function(data) {
- //   $('#current-temperature').text(data.main.temp);
- // })
-
 $('#temperature-up').click(function() {
   thermostat.up();
    updateTemperature();
